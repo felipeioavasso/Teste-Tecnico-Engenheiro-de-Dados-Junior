b034b8270b6aa50702a5eb97f6a34c9511f11ccd
felipeioavasso <felipeioavasso@gmail.com>
2025-03-12 12:32:26 -0300

    atualização da conexão do MongoDB

--- src/database/database_mongo.py
+++ src/database/database_mongo.py
@@ -5,10 +5,6 @@ from dotenv import load_dotenv
 # Carregar as variáveis do .env
 load_dotenv()
 
-""" # Configuração do MongoDB
-MONGO_URI = "mongodb://localhost:27017/"
-DB_NAME = "base_teste_tecnico" """
-
 # Configuração do MongoDB a partir do .env
 MONGO_URI = os.getenv("MONGO_URI")
 
@@ -19,5 +15,4 @@ def get_mongo_client():
 def get_mongo_db():
     """Retorna o banco de dados MongoDB."""
     client = get_mongo_client()
-    #return client[DB_NAME]
     return client.get_database()
